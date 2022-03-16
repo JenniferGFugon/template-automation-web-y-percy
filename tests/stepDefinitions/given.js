97077b7d352869ac8b90a34894ad6abc0af5410a
jfugon <jennifer.guerrero@helloiconic.com>
2022-03-15 18:13:26 -0600

    update packages chrome webdriver and chrome web driver services.

--- tests/stepDefinitions/given.js
+++ tests/stepDefinitions/given.js
@@ -1,27 +1,26 @@
-const { Given } = require('@cucumber/cucumber');
-const googlePage = require('../pages/google.page.js')
-const sauceLabLoginPage = require('../pages/sauceLab.login.page.js')
-const sauceLabHomePage = require('../pages/sauceLab.home.page.js');
+const { Given } = require("@cucumber/cucumber");
+const googlePage = require("../pages/google.page.js");
+const sauceLabLoginPage = require("../pages/sauceLab.login.page.js");
+const sauceLabHomePage = require("../pages/sauceLab.home.page.js");
 
 // Google website
 Given(/^a user at google$/, async function () {
-  await googlePage.open('/')
-  await expect(await browser.getTitle()).to.equal('Google');
+  await googlePage.open("/");
+  await expect(await browser.getTitle()).to.equal("Google");
   // browser.pause(1000);
-})
+});
 
 // Sauce labs demo page
-Given('a user at {string} login page', async function (websiteName) {
-  if ((await browser.getUrl()).includes('inventory.html')) {
+Given("a user at {string} login page", async function (websiteName) {
+  if ((await browser.getUrl()).includes("inventory.html")) {
     // Do logout for each example in the scenario outline, need to start from the login page
-    sauceLabHomePage.doLogout()
-    expect(browser).toHaveTitle(websiteName)
+    sauceLabHomePage.doLogout();
+    expect(browser).toHaveTitle(websiteName);
   } else {
-    await sauceLabLoginPage.open('https://www.saucedemo.com')
-    expect(browser).toHaveTitle(websiteName)
-    await sauceLabLoginPage.usernameInput.waitForClickable()
-    await sauceLabLoginPage.passwordInput.waitForClickable()
-    await sauceLabLoginPage.loginButton.waitForClickable()
+    await sauceLabLoginPage.open("https://www.saucedemo.com");
+    expect(browser).toHaveTitle(websiteName);
+    await sauceLabLoginPage.usernameInput.waitForDisplayed();
+    await sauceLabLoginPage.passwordInput.waitForDisplayed();
+    await sauceLabLoginPage.loginButton.waitForClickable();
   }
-
-})
+});
