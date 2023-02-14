c7e068941c505dadbb7ffaa2df8fe87060a0d353
jfugon <jennifer.guerrero@helloiconic.com>
2023-02-14 16:55:39 -0600

    vvv

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,25 +50,15 @@ exports.config = {
   // https://docs.saucelabs.com/reference/platforms-configurator
   //
   capabilities: [
-    //{
-      // maxInstances: 5,
-      //browserName: "chrome",
-      //acceptInsecureCerts: true,
-      
-    //},
-     {
-         // maxInstances: 5,
-         browserName: "firefox",
-     "moz:firefoxOptions": {
-       args: ["-headless"],
-     },
-
-     },
-    // {
-    //     // maxInstances: 5,
-    //     browserName: 'safari',
-    // }
+    {
+      maxInstances: 5,
+      browserName: "firefox",
+      "moz:firefoxOptions": {
+        args: ["-headless"],
+      },
+    },
   ],
+ 
   //
   // ===================
   // Test Configurations
