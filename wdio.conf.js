c06e36d7372ca3eea671cfe4aef766165bd189ee
jfugon <jennifer.guerrero@helloiconic.com>
2023-02-14 16:25:25 -0600

    cambios 3

--- wdio.conf.js
+++ wdio.conf.js
@@ -61,6 +61,7 @@ exports.config = {
          browserName: "firefox",
      "moz:firefoxOptions": {
        args: ["-headless"],
+       binary: 'C:/Program Files/Firefox Nightly'
      },
 
      },
