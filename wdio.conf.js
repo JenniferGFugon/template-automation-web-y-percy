7b3ce5649e16cd9ad3d8b6644c8950882fd8a18f
jfugon <jennifer.guerrero@helloiconic.com>
2023-02-16 15:42:57 -0600

    add browserstack

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,7 @@
 exports.config = {
+  user:process.env.BROWSERSTACK_USERNAME,
+  key:process.env.BROWSERSTACK_ACCESS_KEY,
+
   //
   // ====================
   // Runner Configuration
@@ -50,18 +53,18 @@ exports.config = {
   // https://docs.saucelabs.com/reference/platforms-configurator
   //
   capabilities: [
-    /*{
+    {
       maxInstances: 5,
       browserName: "chrome",
-    },*/
-    {
+    },
+   /* {
       maxInstances: 5,
       browserName: "firefox",
       "moz:firefoxOptions": {
         args: ["-headless"],
         binary: 'C:/Program Files/Firefox Nightly/firefox.exe',
       },
-    },
+    },*/
   ],
  
   //
@@ -104,8 +107,11 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ["chromedriver"],
-
+  services:[ 
+    //["chromedriver"],
+  ["browserstack"]],
+  host:'hub.browserstack.com',
+  path:'/wd/hub',
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
   // see also: https://webdriver.io/docs/frameworks.html
